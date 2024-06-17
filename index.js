7bfa9099d547d9dd9a8378206d2181c5914d736c
otakugodie <95259091+otakugodie@users.noreply.github.com>
2024-06-17 18:28:56 +0100

    Login OK

--- index.js
+++ index.js
@@ -9,17 +9,27 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello</h1>');
 })
 
-app.post('/login', (req, res) => {})
-app.post('/register', (req, res) => {
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await UserRepository.login({ username, password })
+    res.send({ user })
+  } catch (error) {
+    res.status(401).send(error.message)
+  }
+})
+app.post('/register', async (req, res) => {
   const { username, password } = req.body
   console.log(req.body)
   // console.log({ username, password })
 
   try {
-    const id = UserRepository.create({ username, password })
+    const id = await UserRepository.create({ username, password })
     res.send({ id })
   } catch (error) {
-    res.status(400).send({ error })
+    // console.log(`Error: ${error}`)
+    res.status(400).send(error.message)
   }
 })
 
